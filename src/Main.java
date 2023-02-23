0b3731167d45d9a47f13b6f66c6d7cceae65cbb8
Babbass <Jaycee4005@gmail.com>
2023-02-23 16:40:19 +0300

    Area of  a circle

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        areaOfCircle(8);
+
+    }
+    public static void areaOfCircle(double radius){
+       double area =3.142* radius*radius;
+        System.out.println("The Area of the Circle is:" +area);
+    }
+}
